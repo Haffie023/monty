b25a79e8b011356b73ab2512b6d576a15a5200a1
Haffie023 <joyce.muchoki@gmail.com>
2023-08-24 14:05:13 -0700

    monty

--- swap.c
+++ swap.c
@@ -1,22 +1,28 @@
 #include "monty.h"
 /**
  * swap - swaps the top two elements of the stack.
- * @s: the line number
+ * @stack: pointer to the stack
  * 
- * Return: nothing
+ * @line_number: line number where the call function is called from
 */
 
-void swap(struct stack_s)
-{ 
-    if (s->current_size < 2) 
-    { 
-        printf("L%d: can't swap, stack too short\n", s->line_number);
-        exit(EXIT_FAILURE);
-    }
-    else
-    { 
-        int temp = s->data[s->current_size];
-        s->data[s->current_size] = s->data[s->current_size - 1];
-        s->data[s->current_size - 1] = temp;
-    } 
+void swap(stack_t ** stack, unsigned int line_number)
+{
+	stak_t *tmp1,tmp2;
+	(void) stack;
+	if (args->stack_lengthy <2)
+	{
+		dprintf(2,"L%d: can't swap, stack too short\n",line_number);
+		free_all_args();
+		exit(EXIT_FAILURE);
+	}
+	tmp1 = arguments->head;
+	tmp2 - tmp1->next;
+	tmp1->next = tmp->next;
+	if (tmp1->next)
+		tmp1->next->prev - tmp1;
+	tmp2->next = tmp1;
+	tmp1->prev - tmp2;
+	tmp2->prev = NULL;
+	args->head = tmp2;
 }
